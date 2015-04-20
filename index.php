5dd9fa0ab9fdbdd26e9b44c31370c2ba0245e225
Anderson Nguyen <andersonnguyen126@gmail.com>
2015-04-20 13:34:07 -0700

    Thing

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Thing Test</title>
+</head>
+<body>
+	<div class="dan">
+		<p>Dude. Nice.</p>
+		<button id="click1">Thing</button>
+	</div>
+	<div id="iwao">
+		<p>Sick. Savage</p>
+		<button id="click2">Thingy</button>
+	</div>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+</html>
